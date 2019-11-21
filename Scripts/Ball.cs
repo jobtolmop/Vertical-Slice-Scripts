76b1b0eb493aa50cf5a628d659a4851421344a3e
Everardus Tol <43673344+jobtolmop@users.noreply.github.com>
2019-11-21 11:30:46 +0100

    Add files via upload

---
+++ Scripts/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private bool isPressed;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private Rigidbody2D swing;
+
+    [SerializeField] private float releaseTime = .15f;
+
+    [SerializeField] private float maxDragDistance = 4f;
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector3.Distance(mousePos, swing.position) > maxDragDistance)
+            {
+                rb.position = swing.position + (mousePos - swing.position).normalized * maxDragDistance;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent<TrailRenderer>().enabled = false;
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        gameObject.GetComponent<TrailRenderer>().enabled = true;
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+
+        this.enabled = false;
+    }
+}
---
+++ Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private float health = 4f;
+    void OnCollisionEnter2D(Collision2D colInfo)
+    {
+        if(colInfo.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+        //Debug.Log(colInfo.relativeVelocity.magnitude);
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
